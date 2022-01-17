40f133d7ebc944920873f9b607af56f5b900e456
johannes <johannes.jokinen@metropolia.fi>
2022-01-17 17:26:06 +0200

    task3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,83 @@
 console.log('Hello console!');
 
-const coursesEn = ["Hamburger, cream sauce and poiled potates",
+'use strict';
+
+const coursesEn = [
+  "Hamburger, cream sauce and poiled potates",
   "Goan style fish curry and whole grain rice",
   "Vegan Chili sin carne and whole grain rice",
   "Broccoli puree soup, side salad with two napas",
   "Lunch baguette with BBQ-turkey filling",
-  "Cheese / Chicken / Vege / Halloum burger and french fries"];
-const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+  "Cheese / Chicken / Vege / Halloum burger and french fries"
+];
+
+const coursesFi = [
+  "Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
   "Goalaista kalacurrya ja täysjyväriisiä",
-  "vegaani Chili sin carne ja täysjyväriisi",
+  "Vegaani Chili sin carne ja täysjyväriisi",
   "Parsakeittoa,lisäkesalaatti kahdella napaksella",
   "Lunch baguette with BBQ-turkey filling",
-  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"
+];
 
+const menu = document.querySelector('.menu');
+const lang = document.querySelector('.lang');
+const sort = document.querySelector('.sort');
+const rand = document.querySelector('.random');
+
+let finnish = true;
+
+coursesEn.sort();
+coursesFi.sort();
+
+coursesFi.forEach(annos => {
+  menu.innerHTML += '<li class="">' + annos +'</li>';
+});
+
+const changeLang = () => {
+  menu.innerHTML = '';
+  if (finnish) {
+    coursesEn.forEach(course => {
+      menu.innerHTML += '<li class="">' + course +'</li>';
+    });
+    finnish = false;
+  } else {
+    coursesFi.forEach(annos => {
+      menu.innerHTML += '<li class="">' + annos +'</li>';
+    });
+    finnish = true;
+  };
+};
+
+lang.addEventListener('click', changeLang);
+
+const sortMenu = () => {
+  menu.innerHTML = '';
+  if (finnish) {
+    coursesFi.reverse();
+    coursesFi.forEach(annos => {
+      menu.innerHTML += '<li class="">' + annos +'</li>';
+    });
+  } else {
+    coursesEn.reverse();
+    coursesEn.forEach(course => {
+      menu.innerHTML += '<li class="">' + course +'</li>';
+    });
+  };
+};
+
+sort.addEventListener('click', sortMenu);
+
+const randomCourse = () => {
+  const rand = Math.floor(Math.random()*coursesFi.length);
+  if (finnish) {
+    alert(coursesFi[rand]);
+  } else {
+    alert(coursesEn[rand]);
+  };
+};
+
+rand.addEventListener('click', randomCourse);
 
 
 
