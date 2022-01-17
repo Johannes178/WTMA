8a5b223bb2d2cbc72966163aed5a55125eda2d18
johannes <johannes.jokinen@metropolia.fi>
2022-01-17 17:47:21 +0200

    task4

--- src/index.js
+++ src/index.js
@@ -2,28 +2,21 @@ console.log('Hello console!');
 
 'use strict';
 
-const coursesEn = [
-  "Hamburger, cream sauce and poiled potates",
-  "Goan style fish curry and whole grain rice",
-  "Vegan Chili sin carne and whole grain rice",
-  "Broccoli puree soup, side salad with two napas",
-  "Lunch baguette with BBQ-turkey filling",
-  "Cheese / Chicken / Vege / Halloum burger and french fries"
-];
+import LunchMenu from './assets/LunchMenu.json';
+console.log('menu object', LunchMenu.courses);
 
-const coursesFi = [
-  "Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-  "Goalaista kalacurrya ja täysjyväriisiä",
-  "Vegaani Chili sin carne ja täysjyväriisi",
-  "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-  "Lunch baguette with BBQ-turkey filling",
-  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"
-];
+const coursesEn = [];
+const coursesFi = [];
+
+for (let i in LunchMenu.courses) {
+  coursesFi.push(LunchMenu.courses[i].title_fi);
+  coursesEn.push(LunchMenu.courses[i].title_en);
+}
 
 const menu = document.querySelector('.menu');
 const lang = document.querySelector('.lang');
 const sort = document.querySelector('.sort');
-const rand = document.querySelector('.random');
+const rand = document.querySelector('.rand');
 
 let finnish = true;
 
