b1ede799b8fed8aab71a5d2729e283d02c7b199b
johannes <johannes.jokinen@metropolia.fi>
2022-02-01 13:28:00 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+const createSecretCode = code => {
+  const keyPresses = [];
+  document.addEventListener('keypress', event => {
+    keyPresses.push(event.key);
+    if (keyPresses.length > code.length) {
+    }
+    if (keyPresses.join('') === code){
+      alert('cheats on');
+    }
+  });
+};
+
+createSecretCode('idkfa')
+
+const displayClickCoords = () => {
+  const outputP = document.querySelector('#output');
+  document.addEventListener('dblclick', event => {
+    outputP.textContent = `Doubleclick coords :${event.clientX}, ${event.clientY}`;
+  });
+};
+displayClickCoords();
+
+const reactToTouches = () => {
+  document.querySelector('p').addEventListener('touchstart', event => {
+    console.log('event');
+  });
+};
+
+
 /*author Johannes Jokinen*/
